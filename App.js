7140c61d659cc3934db550fcca7d4161abd35d18
DAN-PC\Daniel <dannash918@gmail.com>
2024-04-06 16:52:07 +1100

    updated with deal

--- App.js
+++ App.js
@@ -1,44 +1,69 @@
 import React from "react"
-import { StyleSheet, View, SafeAreaView, TouchableOpacity, Text } from "react-native"
+import { StyleSheet, View, Pressable, SafeAreaView, Text } from "react-native"
 
-var ws = new WebSocket('wss://vtc5k01404.execute-api.ap-southeast-2.amazonaws.com/test');
+var ws = new WebSocket('wss://kke8tbr1y5.execute-api.ap-southeast-2.amazonaws.com/test/');
 
-const Keyboard = ({onKeyPress}) => {
-  const row1 = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
+const Keyboard = ({cards, onKeyPress}) => {
   return (
     <View style={styles.keyboard}>
       <View style={styles.keyboardRow}>
-        {row1.map(number => (
-          <TouchableOpacity key={number} onPress={() => onKeyPress(number)}>
+        {cards.map(number => (
+          <Pressable key={number} onPress={() => onKeyPress(number)}>
             <View style={styles.key}>
               <Text style={styles.keyLetter}>{number}</Text>
             </View>
-          </TouchableOpacity>
+          </Pressable>
         ))}
       </View>
     </View>
   )
 }
 
+const DealButton = ({onKeyPress}) => {
+  return (
+    <Pressable onPress={() => onKeyPress(7)}>
+      <View style={styles.key}>
+        <Text style={styles.keyLetter}>DEAL CARDS</Text>
+      </View>
+    </Pressable>
+  )
+}
+
 export default function App() {
   const [numbers, setNumbers] = React.useState([])
-  
+  const [cards, setCards] = React.useState([])
+  ws.onopen = (e) => {
+    console.log("Connected")
+  }
+
   const handleKeyPress = (number) => {
+    console.log(number + " pressed")
     jsonData = {"action": "sendmessage", "message": number}
     ws.send(JSON.stringify(jsonData))
-    
-    // console.log(number)
-    // newNumbers = [...numbers]
-    // newNumbers.push(number)
-    // setNumbers(newNumbers)
+  }
+
+  const handleDeal = () => {
+    console.log("Attempting to deal")
+    jsonData = {"action": "deal", "message": "7"}
+    ws.send(JSON.stringify(jsonData))
   }
 
   ws.onmessage = (e) => {
-    number = e.data
-    newNumbers = [...numbers]
-    newNumbers.push(number)
-    setNumbers(newNumbers)
+    response = JSON.parse(e.data)
+    
+    if (response.handler == "deal") {
+      console.log("Message returned: dealing cards")
+      setCards(response.message)
+    }
+    else {
+      console.log("Message returned: playing number")
+      number = response.message
+      newNumbers = [...numbers]
+      newNumbers.push(number)
+      setNumbers(newNumbers)
+    }
+    
+
   };
   
   return (
@@ -49,7 +74,8 @@ export default function App() {
             <Text style={styles.text}>Number is: {number}</Text>
           </View>
         ))}
-      <Keyboard onKeyPress={handleKeyPress}/>
+      <Keyboard onKeyPress={handleKeyPress} cards={cards}/>
+      <DealButton onKeyPress={handleDeal}/>
     </SafeAreaView>
   )
 }
--- readme.md
+++ readme.md
@@ -4,3 +4,8 @@ npx expo start --web
 
 ### Websockets on AWS
 https://docs.aws.amazon.com/apigateway/latest/developerguide/websocket-api-chat-app.html
+
+
+## TODO When Creating Lambda
+ - Give it Permissions in the Edit Configuration (same as sendmessagehandler)
+ - Lambda proxy integration set to true
